f83fbf1d477899ca7a480802dbfbd1286e597a8d
Емельяненко Андрей Сергеевич <Emelyanenko.A.Ser@sberbank.ru>
2021-09-26 16:55:16 +0300

    [Adapt] try to fix R2coefficient

--- src/main/scala/org/mephi/metric/R2coefficient.scala
+++ src/main/scala/org/mephi/metric/R2coefficient.scala
@@ -5,7 +5,7 @@ import scala.math.pow
 
 class R2coefficient(actualExpectedMap: HashMap) extends Quality {
   override def calc(actualExpectedMap: HashMap): Double = {
-    val meanExpected = actualExpectedMap.foldReight(0)(_ + _) / actualExpectedMap.length
+    val meanExpected = actualExpectedMap.values.sum / actualExpectedMap.length
     return (1 - actualExpectedMap.map {
       case(actual, expected) => pow((expected - actual), 2)
     } / actualExpectedMap.map {
