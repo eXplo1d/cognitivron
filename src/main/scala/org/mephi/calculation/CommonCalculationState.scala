421741bd7ba340e5d4e487169acbe4d57b5b1ccc
Николай posmakov <explo1d@Nikolajs-MacBook-Air.local>
2021-09-27 21:53:55 +0300

    Add CM mechanic

--- src/main/scala/org/mephi/calculation/CommonCalculationState.scala
+++ src/main/scala/org/mephi/calculation/CommonCalculationState.scala
@@ -3,7 +3,8 @@ package org.mephi.calculation
 import org.mephi.events.CalculationResultEvent
 
 class CommonCalculationState(formula: Map[String, Double],
-                             const: Double = 0.0) extends CalculationState {
+                             const: Double = 0.0,
+                             m: Double = -0.243) extends CalculationState {
 
   override def applyDelta(delta: Double): Unit = {
 
@@ -12,10 +13,11 @@ class CommonCalculationState(formula: Map[String, Double],
   override def getAttributes: Set[String] = {
     formula.keySet
   }
-
+  // sigmoid func
   override def calculate(attributes: Map[String, CalculationResultEvent], weightCost: Double): Double = {
-    attributes.map {
+    val x = attributes.map {
       kv => formula.getOrElse(kv._1, 0.0) * kv._2.getResult
     }.sum + const + weightCost
+    1.0 / Math.pow(Math.E, (-1) * m * x)
   }
 }
