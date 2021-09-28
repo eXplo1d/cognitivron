080387e6a248b90b3ca4914b56c70cee9d2aca76
Емельяненко Андрей Сергеевич <Emelyanenko.A.Ser@sberbank.ru>
2021-09-28 18:51:40 +0300

    merge

--- src/main/scala/org/mephi/metric/MeanAbsoluteError.scala
+++ src/main/scala/org/mephi/metric/MeanAbsoluteError.scala
@@ -6,10 +6,8 @@ import scala.math.abs
 
 class MeanAbsoluteError(cognitiveMap: CognitiveMap, dataSeq: Seq[(String, Double)]) extends CognitiveMapError {
 
-  override def calc(actualExpectedMap: CognitiveMap): Double = {
-    actualExpectedMap.map {
-      case(actual, expected) => abs(expected - actual)
-    }.mean()
+  override def calc(cognitiveMap: CognitiveMap): Double = {
+
   }
 
   private def train(cognitiveMapTrainer: CognitiveMapTrainer, cognitiveMap: CognitiveMap, dataSeq: Seq[(String, Double)]): Seq[String, Double] = {
