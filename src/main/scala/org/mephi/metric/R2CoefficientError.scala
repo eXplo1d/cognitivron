da2f4e5686408f2c6b38c29e516e67eb9d597d08
Емельяненко Андрей Сергеевич <Emelyanenko.A.Ser@sberbank.ru>
2021-09-29 20:21:12 +0300

    Add R2CoefficientError

---
+++ src/main/scala/org/mephi/metric/R2CoefficientError.scala
@@ -0,0 +1,12 @@
+package org.mephi.metric
+
+import scala.math.pow
+
+object R2CoefficientError extends ErrorMetric {
+
+  override def calc(predicted: Seq[Double], expected: Seq[Double]): Double = {
+    val expectedMean = expected.sum / expected.length
+    (predicted zip expected).map{ case (pre, exp) => pow(pre - exp, 2)}.sum /
+      (predicted zip expected).map{ case (pre, exp) => pow(pre - expectedMean, 2)}.sum
+  }
+}
